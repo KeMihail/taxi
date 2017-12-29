53354abb8cef7dcb26cd4365220df2b39ebd6b7e
Userd-PC\Userd <Mihail4038@gmail.com>
2017-12-29 04:29:13 +0300

    no message

--- parent-project-keiko/dao-keiko/src/main/java/by/itacademy/keikom/taxi/dao/impl/Car2CarOptionDaoImpl.java
+++ parent-project-keiko/dao-keiko/src/main/java/by/itacademy/keikom/taxi/dao/impl/Car2CarOptionDaoImpl.java
@@ -1,5 +1,10 @@
 package by.itacademy.keikom.taxi.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import by.itacademy.keikom.taxi.dao.ICar2CarOptionDao;
 import by.itacademy.keikom.taxi.dao.dbmodel.Car2CarOption;
+import by.itacademy.keikom.taxi.dao.exeption.SQLExecutionException;
 
 public class Car2CarOptionDaoImpl extends AbstractDaoImpl implements ICar2CarOptionDao {
 
@@ -25,8 +31,23 @@ public class Car2CarOptionDaoImpl extends AbstractDaoImpl implements ICar2CarOpt
 
 	@Override
 	public Integer create(Car2CarOption obj) {
-		// TODO Auto-generated method stub
-		return null;
+
+		try (Connection connect = getConnection();
+				PreparedStatement pst = connect.prepareStatement(
+						"insert into car_2_car_option(car_id,car_option_id)values (?,?);",
+						Statement.RETURN_GENERATED_KEYS)) {
+			LOGGER.info("execute SQL: create new Car2CarOption");
+			pst.setInt(1, obj.getCarId());
+			pst.setInt(2, obj.getCarOptionId());
+			pst.executeUpdate();
+
+			ResultSet rs = pst.getGeneratedKeys();
+			rs.next();
+			Integer id = rs.getInt("id");
+			return id;
+		} catch (SQLException e) {
+			throw new SQLExecutionException(e);
+		}
 	}
 
 	@Override
